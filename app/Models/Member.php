0854158343e9a7ef5362e4a3b71b0e505b7ab1b3
Onso37 <afonso.vo@gmail.com>
2023-11-11 11:32:08 +0000

    continue member model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Member extends Model
 {
@@ -22,5 +23,25 @@ class Member extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function worlds(): BelongsToMany
+    {
+        return $this->belongsToMany(World::class);
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
+
+    public function tasks(): BelongsToMany
+    {
+        return $this->belongsToMany(Task::class, 'assignee');
+    }
+
+    public function worldComments(): HasMany
+    {
+        return $this->hasMany(WorldComment::class);
     }
 }
