84a2efafaeb9b3faad37527060560bc8cc44346b
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-11-12 18:49:13 +0000

    edit member function

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,28 @@ class MemberController extends Controller
         $id = Auth::id();
         $worlds = Member::findOrFail($id)->worlds;
         return view('pages.myworlds', ['worlds' => $worlds]);
+    }
+
+    public function update(Request $request): void
+    {
+        $fields = $request->validate([
+           'birthday' => ['required, date_format:YY-MM-DD'],
+           'name' => ['required', 'alpha_num:ascii'],
+            'description' => ['required', 'alpha_num:ascii'],
+            'email' => ['required', 'email'],
+            'id' => ['required', 'integer', 'numeric']
+        ]);
+
+        $member = Member::findOrFail($fields['id']);
+
+        $this->authorize('edit', $member);
+
+        $member->birthday = $fields['birthday'];
+        $member->username = $fields['username'];
+        $member->description = $fields['description'];
+        $member->email = $fields['email'];
+
+        $member->save();
     }
 
     
