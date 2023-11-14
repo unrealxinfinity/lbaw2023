25b3f36d6421a5b2e34dc2a131bfb0ea0266924c
up202108810 <up202108810@fe.up.pt>
2023-11-14 00:41:23 +0000

    Task Policy and Assign member to task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Models\Member;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\Rule;
 
@@ -14,7 +15,7 @@ class TaskController extends Controller
         $fields = $request->validate([
             'title' => ['alpha_num:ascii'],
             'description' => ['string'],
-            'status' => ['string'],
+            'status' => [Rule::in('BackLog', 'Upcoming', 'In Progress', 'Finalizing', 'Done')],
             'due_at' => ['date'],
             'effort' => ['integer'],
             'priority' => ['string'],
@@ -29,5 +30,24 @@ class TaskController extends Controller
         ]);
 
         return redirect()->route('tasks/' . $task->id)->withSuccess('New Task created!');
+    }
+
+    public function assignMember(Request $request, string $task_id, string $member_id): void
+    {
+        // em cima eu faço project_id, bastava fazer isto?
+        $fields = $request->validate([
+            'type' => [Rule::in('Member', 'Project Leader')]
+        ]);
+
+        $task = Task::findOrFail($task_id);
+        $member = Member::findOrFail($member_id);
+
+        $this->authorize('assignMember', $task);
+
+        $is_admin = $member->worlds->where('id', $task->world_id)[0]->pivot->is_admin;
+        $type = $is_admin ? 'World Administrator' : $fields['type'];
+
+        
+        $member->tasks->attach($task_id, 'assignee');
     }
 }
---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Task;
+
+class TaskPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function assignMember(User $user, Task $task): bool
+    {
+        return ($user->persistentUser->member->projects->where('id', $task->project_id)[0]->pivot->permission_level) == 'Project Leader' || ($user->persistentUser->member->projects->where('id', $task->project_id)[0]->pivot->permission_level) == 'Member' ;
+    }
+}
