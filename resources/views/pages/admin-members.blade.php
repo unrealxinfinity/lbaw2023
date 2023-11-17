ff7a6f1cfc4be41b0cf90a067ecb8609ed293c5b
Onso37 <afonso.vo@gmail.com>
2023-11-17 19:54:52 +0000

    move form

--- resources/views/pages/admin-members.blade.php
+++ resources/views/pages/admin-members.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Administer members')
 
 @section('content')
+    <form method="GET" action="/admin">
+        <input type="text" name="search" placeholder="Search by email/username">
+        <input type="submit" value="Search">
+    </form>
     <section id="members">
         @each('partials.member-edit', $members, 'member')
     </section>
--- resources/views/pages/member-edit.blade.php
+++ resources/views/pages/member-edit.blade.php
@@ -3,9 +3,6 @@
 @section('title', $member->persistentUser->user->username)
 
 @section('content')
-    <form method="GET" action="/admin">
-        <input type="text" name="search" placeholder="Search by email/username">
-    </form>
     <section id="members">
         @include('partials-member-edit', ['member' => $member])
     </section>
