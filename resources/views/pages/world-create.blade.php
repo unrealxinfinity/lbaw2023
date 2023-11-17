d90cffbd8e4bafeb3f85ec68a0e1fb8cbb2e0918
Onso37 <afonso.vo@gmail.com>
2023-11-17 14:17:25 +0000

    add error messages to world creation

--- resources/views/pages/world-create.blade.php
+++ resources/views/pages/world-create.blade.php
@@ -10,7 +10,17 @@
         @method('POST')
 
         <input type="text" name="name" placeholder="New World Name">
+        @if ($errors->has('name'))
+        <span class="error">
+          {{ $errors->first('name') }}
+        </span>
+        @endif
         <input type="text" name="description" placeholder="Description">
+        @if ($errors->has('description'))
+        <span class="error">
+          {{ $errors->first('description') }}
+        </span>
+        @endif
 
         <button type="submit">Create</button>
 
