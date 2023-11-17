577942925e0f420566eb23fa52872013ce632853
up202108810 <up202108810@fe.up.pt>
2023-11-17 15:06:14 +0000

    Request and Controller done

--- app/Http/Controllers/WorldController.php
+++ app/Http/Controllers/WorldController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\World;
+use App\Models\User;
+use App\Http\Requests\AddMemberToWorldRequest;
 use App\Http\Requests\CreateWorldRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
@@ -33,5 +36,25 @@ class WorldController extends Controller
         ]);
 
         return to_route('worlds.show', ['id' => $world->id])->withSuccess('New World created!');
+    }
+
+    public function addMember(AddMemberToWorldRequest $request,string $world_id, string $username): JsonResponse
+    {
+        $fields = $request->validated();
+
+        $world = World::findOrFail($world_id);
+        $member = User::where('username', $username)->first()->persistentUser->member;
+
+        $is_admin = $member->worlds->where('id', $world->id)[0]->pivot->is_admin;
+        $type = $is_admin ? 'World Administrator' : $fields['type'];
+
+        $member->worlds()->attach($world_id, ['permission_level' => $type]);
+
+        return response()->json([
+            'id' => $member->id,
+            'username' => $username,
+            'email' => $member->email,
+            'permission_level' => $type
+        ]);
     }
 }
---
+++ app/Http/Requests/AddMemberToWorldRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\World;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class AddMemberToWorldRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user()->can('addMember', World::find($this->route()->id));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => [Rule::in('Member', 'World Administrator')],
+            'id' => ['exists:App\Models\World,id'],
+            'username' => ['exists:App\Models\User,username']
+        ];
+    }
+}
