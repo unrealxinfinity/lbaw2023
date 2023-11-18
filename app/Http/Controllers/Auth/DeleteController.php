114ee9a436ef81a0d4defbb9624b7573a255c804
Onso37 <afonso.vo@gmail.com>
2023-11-18 16:18:53 +0000

    finish controller method

--- app/Http/Controllers/Auth/DeleteController.php
+++ app/Http/Controllers/Auth/DeleteController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DeleteMemberRequest;
+use App\Models\Member;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -10,6 +13,24 @@ class DeleteController extends Controller
 {
     public function delete(DeleteMemberRequest $request, string $id): RedirectResponse
     {
+        $request->validated();
 
+        $member = Member::findOrFail($id);
+        $persistentUser = $member->persistentUser;
+        $user = $member->persistentUser->user;
+
+        $persistentUser->type_ = 'Deleted';
+
+        $user->delete();
+
+        $member->name = 'deleted';
+        $member->birthday = null;
+        $member->description = null;
+        $member->email = null;
+        $member->picture = null;
+
+        $member->save();
+
+        return redirect()->route('home')->withSuccess('Account deleted.');
     }
 }
--- app/Http/Requests/DeleteMemberRequest.php
+++ app/Http/Requests/DeleteMemberRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Member;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,7 +13,7 @@ class DeleteMemberRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return Auth::user()->can('edit', $this->route()->id);
+        return Auth::user()->can('edit', Member::find($this->route()->id));
     }
 
     /**
