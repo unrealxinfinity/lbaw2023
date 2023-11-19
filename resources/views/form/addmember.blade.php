d5d35ca48447049f273cbf70147003eb6bd63362
isabel <up202108767@up.pt>
2023-11-19 17:23:15 +0000

    empty form input fix

--- resources/views/form/addmember.blade.php
+++ resources/views/form/addmember.blade.php
@@ -1,7 +1,7 @@
 <form id="add-member" class="add-member">
      @csrf
     <input type="hidden" class="id" name="id" value="{{ $project->id }}">
-    <input type="text" class="username" name="username" placeholder="Username">
+    <input type="text" class="username" name="username" placeholder="Username" required>
     <select name="type" class="type">
         <option value="Member">Member</option>
         <option value="Project Leader">Project Leader</option>
