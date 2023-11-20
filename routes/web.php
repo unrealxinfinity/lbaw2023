390a7a929a2d1fcb54e3bc288ed1c832a92c12e9
HaoChang Fu <fuhaochang664961423@hotmail.com>
2023-11-20 00:57:22 +0000

    fixing Search again

--- routes/web.php
+++ routes/web.php
@@ -33,12 +33,12 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', function () {
     return view('pages.homepage');
 })->name('home');
-Route::view('/create-world', 'pages.world-create')->name('world-create');
+Route::view('/create-world', 'pages.world-create')->name('world-create');//
 
 // Cards
 
 Route::controller(SearchController::class)->group(function() {
-    Route::get('/search', 'show')->name('search');
+    Route::get('/search', 'show')->name('search');//
 });
 
 Route::controller(CardController::class)->group(function () {
@@ -50,8 +50,8 @@ Route::controller(WorldController::class)->group(function () {
     Route::post('/api/worlds/{id}/{username}', 'addMember');//
     Route::get('/worlds/{id}', 'show')->name('worlds.show');//
     Route::post('/worlds', 'create')->name('create-world');//
-    Route::get('worlds/{id}/create-project', 'showProjectCreate')->name('project-create');
-    Route::get('/api/worlds/{id}/projects', 'searchProjects')->name('search-projects');
+    Route::get('worlds/{id}/create-project', 'showProjectCreate')->name('project-create');//
+    Route::get('/api/worlds/{id}/projects', 'searchProjects')->name('search-projects');//
     Route::post('/worlds/{id}/comment', 'comment')->name('world-comment');//
 });
 
@@ -60,7 +60,7 @@ Route::controller(ProjectController::class)->group(function () {
     Route::get('/projects/{id}', 'show')->name('projects.show');//
     Route::delete('/projects/{id}', 'delete')->name('delete-project');//
     Route::post('/projects', 'create')->name('create-project');//
-    Route::get('/api/projects/{id}/tasks', 'searchTask')->name('search-tasks');
+    Route::get('/api/projects/{id}/tasks', 'searchTask')->name('search-tasks');//
 });
 
 Route::controller(TagController::class)->group(function () {
@@ -70,8 +70,8 @@ Route::controller(TagController::class)->group(function () {
 Route::controller(MemberController::class)->group(function () {
     Route::get('members/{username}', 'show'); //
     Route::get('/myworlds', 'showMemberWorlds');
-    Route::get('/myprojects', 'showMemberProjects');
-    Route::get('/mytasks', 'showMemberTasks');
+    Route::get('/myprojects', 'showMemberProjects');//
+    Route::get('/mytasks', 'showMemberTasks');//
     Route::put('/api/members/{id}', 'update')->name('update-member');
     Route::get('/admin', 'list')->name('list-members');
     Route::get('/members/{username}/edit', 'showEditProfile')->name('edit-member');
@@ -80,7 +80,7 @@ Route::controller(MemberController::class)->group(function () {
 Route::controller(TaskController::class)->group(function () {
     Route::post('/tasks/create', 'create')->name('create-task');//
     Route::get('/tasks/{id}', 'show')->name('tasks.show');//
-    Route::post('/tasks/{id}/complete', 'complete')->name('complete-task');
+    Route::post('/tasks/{id}/complete', 'complete')->name('complete-task');//
     Route::post('/tasks/{id}', 'edit')->name('edit-details');//
     Route::post('/api/tasks/{id}/{username}', 'assignMember');
     Route::post('/tasks/{id}/comment', 'comment')->name('task-comment');
