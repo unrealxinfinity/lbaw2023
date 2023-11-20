5ca449726da1e4dbf91357dd71ba01d42d7340b7
Onso37 <afonso.vo@gmail.com>
2023-11-20 19:34:51 +0000

    prefix search on main search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -23,22 +23,29 @@ class SearchController extends Controller
         $searchedText = strip_tags($searchedText);    
         $member = Member::where('user_id', auth()->user()->id)->first(); 
         $id = $member->user_id;
+        $arr = explode(' ', $searchedText);
+        for ($i = 0; $i < count($arr); $i++) {
+            $arr[$i] = $arr[$i] . ':*';
+        }
+        $searchedText = implode(' | ', $arr);
+        
+        error_log($searchedText);
         $tasks = $member->tasks()->select('id','title','description','due_at','status','effort','priority')
-            ->whereRaw("searchedTasks @@ plainto_tsquery('english', ?) AND project_id = ?", [$searchedText, $id])
-            ->orderByRaw("ts_rank(searchedTasks, plainto_tsquery('english', ?)) DESC", [$searchedText])
+            ->whereRaw("searchedTasks @@ to_tsquery('english', ?) AND project_id = ?", [$searchedText, $id])
+            ->orderByRaw("ts_rank(searchedTasks, to_tsquery('english', ?)) DESC", [$searchedText])
             ->get();
         $projects = $member->projects()->select('id', 'name', 'description', 'status', 'picture')
-            ->whereRaw("searchedProjects @@ plainto_tsquery('english', ?)", [$searchedText])
-            ->orderByRaw("ts_rank(searchedProjects, plainto_tsquery('english', ?)) DESC", [$searchedText])
+            ->whereRaw("searchedProjects @@ to_tsquery('english', ?)", [$searchedText])
+            ->orderByRaw("ts_rank(searchedProjects, to_tsquery('english', ?)) DESC", [$searchedText])
             ->get();
         $members = Member::select('members.user_id', 'members.picture', 'user_info.username','members.name', 'members.email', 'members.birthday', 'members.description')
         ->join('user_info', 'members.user_id', '=', 'user_info.id')
-        ->whereRaw('searchMembers @@ plainto_tsquery(\'english\', ?) OR searchUsername @@ plainto_tsquery(\'english\', ?)', [$searchedText,$searchedText])
-        ->orderByRaw('ts_rank(searchMembers, plainto_tsquery(\'english\', ?)), ts_rank(searchUsername, plainto_tsquery(\'english\', ?)) DESC', [$searchedText,$searchedText])
+        ->whereRaw('searchMembers @@ to_tsquery(\'english\', ?) OR searchUsername @@ to_tsquery(\'english\', ?)', [$searchedText,$searchedText])
+        ->orderByRaw('ts_rank(searchMembers, to_tsquery(\'english\', ?)), ts_rank(searchUsername, to_tsquery(\'english\', ?)) DESC', [$searchedText,$searchedText])
         ->get();
         $worlds = World::select('id', 'picture','name', 'description')
-            ->whereRaw("tsvectors @@ plainto_tsquery('english', ?)", [$searchedText])
-            ->orderByRaw("ts_rank(tsvectors, plainto_tsquery('english', ?)) DESC", [$searchedText])
+            ->whereRaw("tsvectors @@ to_tsquery('english', ?)", [$searchedText])
+            ->orderByRaw("ts_rank(tsvectors, to_tsquery('english', ?)) DESC", [$searchedText])
             ->get();
         
         return view('pages.search', [
