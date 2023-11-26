f1c0cbe55de17a5dbba370710d7519c984e0f6c3
isabel <up202108767@up.pt>
2023-11-26 04:13:47 +0000

    changed my pages layout

--- resources/views/partials/mytasks.blade.php
+++ resources/views/partials/mytasks.blade.php
@@ -1,8 +1,8 @@
-<article class="mytask" data-id="{{ $task->id }}">
-    <div class="row">
-        <div class="column">
-            <h2><a href="/tasks/{{ $task->id }}">{{ $task->title }}</a></h2>
-            <h4> {{ $task->description }} </h4>
+<article class="mytask bg-grey rounded m-5" data-id="{{ $task->id }}">
+    <div class="flex">
+        <div class="flex flex-col">
+            <h1 class="text-white"><a href="/tasks/{{ $task->id }}">{{ $task->title }}</a></h2>
+            <h2 class="ml-3 mb-5"> {{ $task->description }} </h4>
         </div>
     </div>
 </article>
