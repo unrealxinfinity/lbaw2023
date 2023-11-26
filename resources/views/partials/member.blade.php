22878a7ce106d669045da6563fdba3a83de7b68d
isabel <up202108767@up.pt>
2023-11-26 04:07:17 +0000

    changed profile page layout

--- resources/views/partials/member.blade.php
+++ resources/views/partials/member.blade.php
@@ -1,19 +1,21 @@
 <article class="member" data-id="{{ $member->id }}">
     @if($main)
     <p><a href="/">Home</a> > <a href="/members/{{$member->persistentUser->user->username}}">{{$member->persistentUser->user->username}}</a></p>
-    <header class="row">
-        <img src= {{$member->picture}} class="big">
-        <div class="column">
-        <h2>{{ $member->name }}</h2>
-        <h3> @ {{ $member->persistentUser->user->username }}</h3>
+    <header class="flex justify-start sm:h-40 h-24 m-5">
+        <img src= {{$member->picture}} class="pfp">
+        <div class="flex flex-col ml-5 pt-1">
+        <h1>{{ $member->name }}</h1>
+        <h2 class="pl-3 mb-5"> @ {{ $member->persistentUser->user->username }}</h2>
+        <div class="flex"> <p class="tag"> placeholder </p> <p class="tag"> for tags </p>
+        </div>
         </div>
     </header>
-    <h4>{{ $member->description }}</h4>
+    <h2 class="mb-5">{{ $member->description }}</h2>
     @if (Auth::check() && Auth::user()->id == $member->persistentUser->user->id)
         <a class="button" href="/members/{{ $member->persistentUser->user->username }}/edit">Edit Profile</a>
-        <h3> <a href = "/myworlds" >My Worlds</a></h3>
-        <h3> <a href="/myprojects">My Projects</a></h3>
-        <h3> <a href="/mytasks">My Tasks</a></h3>
+        <h1> <a href = "/myworlds" >My Worlds</a></h1>
+        <h1> <a href="/myprojects">My Projects</a></h1>
+        <h1> <a href="/mytasks">My Tasks</a></h1>
     @endif
     @else
     <header class="flex justify-start">
