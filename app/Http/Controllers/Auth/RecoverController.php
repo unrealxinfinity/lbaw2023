51f2c8db06e5d49b8ce8addafa2c599ef0b0376c
Onso37 <afonso.vo@gmail.com>
2023-12-01 11:11:56 +0000

    add token validation

--- app/Http/Controllers/Auth/RecoverController.php
+++ app/Http/Controllers/Auth/RecoverController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RecoverRequest;
+use App\Http\Requests\ResetRequest;
 use App\Mail\MailModel;
 use App\Models\Member;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
@@ -20,12 +22,12 @@ class RecoverController extends Controller
         }
     }
 
-    function showResetForm()
+    function showResetForm(Request $request)
     {
         if (Auth::check()) {
             return redirect('');
         } else {
-            return view('auth.reset');
+            return view('auth.reset', ['id' => $request->input('id'), 'token' => $request->input('token')]);
         }
     }
 
@@ -37,14 +39,29 @@ class RecoverController extends Controller
 
         $member = Member::where('email', $fields['email'])->first();
 
-        $member->recoverToken = $recoverToken;
+        $member->token = $recoverToken;
+        $member->save();
 
         $mailData = [
             'name' => $member->name,
-            'link' => env('APP_URL') . '/reset-password?id=' . $member->id . '&token=' . $recoverToken
+            'link' => env('APP_URL') . '/reset?id=' . $member->id . '&token=' . $recoverToken
         ];
 
         Mail::to($member->email)->send(new MailModel($mailData));
         return redirect()->back()->withSuccess('Email sent to provided address. Please check your inbox');
+    }
+
+    function reset(ResetRequest $request)
+    {
+        error_log("soup");
+        $fields = $request->validated();
+
+        $member = Member::findOrFail($fields['id']);
+        $member->token = null;
+        $member->save();
+
+        $user = $member->persistentUser->user;
+
+        error_log("sup");
     }
 }
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -4,7 +4,8 @@
 <form method="POST" class="form-post" action="{{ route('reset-password') }}">
     @csrf
 
-    <input type="hidden" name="member" value="on">
+    <input type="hidden" name="token" value={{ $token }}>
+    <input type="hidden" name="id" value={{ $id }}>
 
     <label for="password">New Password</label>
     <input id="password" type="password" name="password" required>
