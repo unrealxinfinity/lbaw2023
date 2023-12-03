baf1e86e4b427f96e9e9416489008f499ec93c29
Onso37 <afonso.vo@gmail.com>
2023-12-03 23:14:01 +0000

    fix account deletion on certain browsers

--- app/Http/Controllers/Auth/DeleteController.php
+++ app/Http/Controllers/Auth/DeleteController.php
@@ -37,7 +37,7 @@ class DeleteController extends Controller
     public function showConfirmation(Request $request, string $username)
     {
         $member = User::where('username', $username)->first()->persistentUser->member;
-        $url = $request->session()->previousUrl();
+        $url = str_replace('localhost', '127.0.0.1', $request->session()->previousUrl());
         error_log($url);
         $expected = str_replace('localhost', '127.0.0.1', env('APP_URL') . '/members/' . $username);
         error_log($expected);
