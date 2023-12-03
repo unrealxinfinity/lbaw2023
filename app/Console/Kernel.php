6442480e529468ec09348feca86325e5473718ff
Onso37 <afonso.vo@gmail.com>
2023-12-03 19:59:19 +0000

    add schedule function

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,8 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Models\Task;
+use App\Models\Notification;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -12,7 +14,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            $tomorrow = date_add(date_time_set(new DateTime(), 0, 0), new DateInterval("P1D"));
+            $tasks = Task::all();
+            $tasks = array_filter($tasks, function($task) {
+                return (new DateTime($task->due_at)) == $tomorrow;
+            });
+            foreach ($tasks as $task) {
+                $notification = Notification::create([
+                    'text' => "Task $task->title due tomorrow!",
+                    'level' => 'High',
+                    'task_id' => $task->id
+                ]);
+
+                foreach ($tasks->assigned as $assignee) {
+                    $assignee->notifications()->attach($notification->id);
+                }
+            }
+        })->daily();
     }
 
     /**
