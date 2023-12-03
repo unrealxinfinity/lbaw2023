fa9081b24a961b3f2b195e99ba3113c61032d577
up202108810 <up202108810@fe.up.pt>
2023-12-03 21:36:09 +0000

    Readded the forms with their proper viewing permissions

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,29 +5,36 @@
     <h1>Members</h1>
     <ul class="members">
         @if ($type == 'project')
-        <h2 class="text-grey font-semibold"> Project Leaders </h2>
-        @foreach($thing->members()->where('permission_level', '=', 'Project Leader')->orderBy('id')->get() as $member)
-            @include('partials.member', ['member' => $member, 'main' => false])
-            @include('form.remove-member', ['thing' => $thing, 'member' => $member])
-        @endforeach
-        <h2 class="mt-5 text-grey font-semibold"> Members </h2>
-        @foreach($thing->members()->where('permission_level', '=', 'Member')->orderBy('id')->get() as $member)
-            @include('partials.member', ['member' => $member, 'main' => false])
-        @endforeach
+            <h2 class="text-grey font-semibold"> Project Leaders </h2>
+            @foreach($thing->members()->where('permission_level', '=', 'Project Leader')->orderBy('id')->get() as $member)
+                @include('partials.member', ['member' => $member, 'main' => false])
+            @endforeach
+            <h2 class="mt-5 text-grey font-semibold"> Members </h2>
+            @foreach($thing->members()->where('permission_level', '=', 'Member')->orderBy('id')->get() as $member)
+                @include('partials.member', ['member' => $member, 'main' => false])
+                @if (Auth::check() && Auth::user()->persistentUser->member->projects->where('id', $thing->id)->first()->pivot->permission_level=="Project Leader")
+                    @include('form.remove-member', ['thing' => $thing, 'member' => $member])
+                @endif
+            @endforeach
         @endif
         @if ($type == 'world')
-        <h2 class="text-grey font-semibold"> World Owner </h2>
-            @include('partials.member', ['member' => $thing->owner()->get()->first(), 'main' => false])
-        <h2 class="mt-5 text-grey font-semibold"> World Admins </h2>
-        @foreach($thing->members()->where('is_admin', '=', 'true')->orderBy('id')->get() as $member)
-            @if ($member->id != $thing->owner()->get()->first()->id)
-            @include('partials.member', ['member' => $member, 'main' => false])
-            @endif
-        @endforeach
-        <h2 class="mt-5 text-grey font-semibold"> Members </h2>
-        @foreach($thing->members()->where('is_admin', '=', 'false')->orderBy('id')->get() as $member)
-            @include('partials.member', ['member' => $member, 'main' => false])
-        @endforeach
+            <h2 class="text-grey font-semibold"> World Owner </h2>
+                @include('partials.member', ['member' => $thing->owner()->get()->first(), 'main' => false])
+            <h2 class="mt-5 text-grey font-semibold"> World Admins </h2>
+            @foreach($thing->members()->where('is_admin', '=', 'true')->orderBy('id')->get() as $member)
+                @if ($member->id != $thing->owner()->get()->first()->id)
+                    @include('partials.member', ['member' => $member, 'main' => false])
+                @elseif (Auth::check() && Auth::user()->persistentUser->member->worlds->where('id', $thing->id)->first()->pivot->is_admin && Auth::user()->persistentUser->member->id !== $member->id)
+                    @include('partials.member', ['member' => $member, 'main' => false])
+                @endif
+            @endforeach
+            <h2 class="mt-5 text-grey font-semibold"> Members </h2>
+            @foreach($thing->members()->where('is_admin', '=', 'false')->orderBy('id')->get() as $member)
+                @include('partials.member', ['member' => $member, 'main' => false])
+                @if(Auth::check() && Auth::user()->persistentUser->member->worlds->contains('id', $thing->id) && Auth::user()->persistentUser->member->worlds->where('id', $thing->id)->first()->pivot->is_admin)
+                    @include('form.remove-member', ['thing' => $thing, 'member' => $member])
+                @endif
+            @endforeach
         @endif
     </ul>
 </article>
