c74027aa2615083f9a5a3db7d6a8edd4075d8bdc
up202108810 <up202108810@fe.up.pt>
2023-12-04 20:25:36 +0000

    Join world request

---
+++ app/Http/Requests/JoinWorldRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class JoinWorldRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'exists:App\Models\Member,id'],
+            'is_admin' => ['required', 'boolean'],
+            'world_id' => ['required', 'exists:App\Models\World,id'],            
+            'invite_token' => ['required' ,Rule::exists('members')->where(function (Builder $query) {
+                return $query->where('id', request()->id);
+            })]
+        ];
+    }
+}
