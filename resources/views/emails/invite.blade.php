deb4957ac01055f9a329a2ada8dbf3458507f0b5
up202108810 <up202108810@fe.up.pt>
2023-12-04 20:55:25 +0000

    invite email content

---
+++ resources/views/emails/invite.blade.php
@@ -0,0 +1,5 @@
+<h3>Hi {{ $mailData['name'] }},</h3>
+<h4>It seems you were invited to join {{ $mailData['world_name']}}.</h4>
+<p>To join, <a href={{ $mailData['link'] }}>click here</a>. If you do not wish to join this world please ignore this email.</p>
+<h5>Regards,</h5>
+<h5>The MineMax staff</h5>
