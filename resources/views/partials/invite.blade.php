18057103954c52455695087ea4ab916e17bda1d7
up202108810 <up202108810@fe.up.pt>
2023-12-05 13:20:13 +0000

    World policy fix

---
+++ resources/views/partials/invite.blade.php
@@ -0,0 +1,7 @@
+<article class="invite">
+
+    <h1>Invite</h1>
+    <h2>Do you wish to join <a href="/worlds/{{request('wid')}}">{{}}</a></h2>
+    <button>Yes</button> <button>No</button>
+
+</article>
