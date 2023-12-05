7fac4ea0056d6fb11e3457dd808a5cc3f2b7d8f2
up202108810 <up202108810@fe.up.pt>
2023-12-05 13:45:25 +0000

    Accept invite form

--- resources/views/partials/invite.blade.php
+++ resources/views/partials/invite.blade.php
@@ -8,8 +8,17 @@
         <input type="hidden" class="world_id" name="world_id" value="{{ $world_id }}">
         <input type="hidden" class="username" name="username" value="{{ $username }}">
         <input type="hidden" class="type" name="type" value="{{ $type }}">
+        <input type="hidden" class="acceptance" name="acceptance" value="true">
         <input class="button" type="submit" value="Yes">
     </form>
-    <button>Yes</button> <button>No</button>
+    <form class="form-post" action={{ route ('join-world', ['id' => $world_id]) }} method="POST">
+        @csrf
+        @method('POST')
+        <input type="hidden" class="world_id" name="world_id" value="{{ $world_id }}">
+        <input type="hidden" class="username" name="username" value="{{ $username }}">
+        <input type="hidden" class="type" name="type" value="{{ $type }}">
+        <input type="hidden" class="acceptance" name="acceptance" value="false">
+        <input class="button" type="submit" value="No">
+    </form>
 
 </article>
