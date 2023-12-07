1099291f7c8198713d13f6aa6674b3141b858ea5
up202108810 <up202108810@fe.up.pt>
2023-12-07 02:42:18 +0000

    View fix

--- resources/views/partials/invites.blade.php
+++ resources/views/partials/invites.blade.php
@@ -4,7 +4,7 @@
         <img class="h-16 aspect-square mt-5 ml-5 ">
         <div class="flex flex-col">
             <h1 class="text-white">You have been invited to join <a href="worlds/{{$invite->world_id}}">{{$invite->world_id}}</a></h1>
-            <h2 class="ml-3 mb-5"> Do you wish to accept?</h2>
+            <h2 class="ml-3 mb-5"> <a href="">Check Invite</a></h2>
         </div>
     </div>
 </article>
