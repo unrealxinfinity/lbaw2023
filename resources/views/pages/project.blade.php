24f3d2293c50f9694a92cc4cd9515e1999972a97
isabel <up202108767@up.pt>
2023-12-08 14:28:51 +0000

    world/project sidebar fix

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -7,10 +7,10 @@
     <section id="projects" class="desktop:flex">
         <input type="checkbox" id="show-details" class="hidden peer"/>
         @include('partials.project', ['project' => $project, 'tags' => $tags])
-        <div class="md:hidden fixed bg-opacity-95 bg-black text-white top-28 h-full w-0 right-0 peer-checked:w-full peer-checked:pl-5 transition-width duration-500 overflow-hidden z-10">
+        <div class="desktop:hidden fixed bg-opacity-95 bg-black top-0 h-full w-0 right-0 mobile:peer-checked:w-2/3 tablet:peer-checked:w-1/2 peer-checked:w-full peer-checked:px-5 transition-width duration-500 z-10">
             @include('partials.sidebar', ['thing'=>$project, 'type' => 'project'])
         </div>
-        <div class="hidden md:contents">
+        <div class="hidden desktop:contents">
             @include('partials.sidebar', ['thing'=>$project, 'type' => 'project'])
         </div>
     </section>
