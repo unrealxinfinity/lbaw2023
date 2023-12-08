091cb4ccd72d84486902355d48a0870eea49aeb3
up202108810 <up202108810@fe.up.pt>
2023-12-08 01:01:48 +0000

    CSS fix

--- resources/views/pages/worlds.blade.php
+++ resources/views/pages/worlds.blade.php
@@ -4,10 +4,8 @@
 
 @section('content')
     <p><a href="/">Home</a> > <a href="/worlds"> Worlds</a></p>
-    <section id="worlds" class="md:flex justify-start">
-        @foreach ($worlds as $world)
-            @include('partials.myworlds', ['world' => $world])
-        @endforeach
+    <section id="worlds">
+        @each('partials.myworlds', $worlds, 'world')
     </section>
 
 @endsection
