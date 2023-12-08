b50358ce515a39ddc1066e2c0f271b6a8bfb0924
Onso37 <afonso.vo@gmail.com>
2023-12-08 12:56:35 +0000

    first title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title') - {{env('APP_NAME')}} </title>
 
         <!-- Styles -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
