aa19d611934d2c20038add04ce40af8932fd2748
up202108810 <up202108810@fe.up.pt>
2023-12-09 17:06:07 +0000

    Better view for worlds delete form

--- resources/views/partials/myworlds.blade.php
+++ resources/views/partials/myworlds.blade.php
@@ -1,25 +1,27 @@
 <article class="myworld bg-grey rounded m-5" data-id="{{ $world->id }}">
-    <div class="flex">
-        <img src={{$world->getImage()}} class="h-16 aspect-square mt-5 ml-5 ">
-        <div class="flex flex-col">
-            <h1 class="text-white"><a href="/worlds/{{ $world->id }}">{{ $world->name }}</a></h1>
-            <h2 class="ml-3 mb-5"> {{ $world->description }} </h2>
+    <div class="flex justify-between">
+        <div class="flex m-3">
+            <img src={{$world->getImage()}} class="h-16 aspect-square mr-3">
+            <div class="self-center">
+                <h1 class="text-white"><a href="/worlds/{{ $world->id }}">{{ $world->name }}</a></h1>
+                <h2 class=""> {{ $world->description }} </h2>
+            </div>
         </div>
         @if(Auth::check() && Auth::user()->can('leave', $world) || Auth::user()->can('delete', $world))
-            <input type="checkbox" id="more-options-{{$world->id}}" class="hidden peer"/>
-            <label for="more-options-{{$world->id}}" class="text-start font-bold md:text-big text-bigPhone h-fit my-3 sm:mr-5 cursor-pointer">&#8942;</label>
-            <div class="absolute right-0 z-10 w-40 sm:mr-5 px-2 rounded bg-grey peer-checked:block hidden divide-y divide-white divide-opacity-25">
-                @if(Auth::check() && Auth::user()->can('delete', $world))
+        <input type="checkbox" id="more-options-{{$world->id}}" class="hidden peer"/>
+        <label for="more-options-{{$world->id}}" class="text-start font-bold md:text-big text-bigPhone h-fit my-3 sm:mr-5 cursor-pointer">&#8942;</label>
+        <div class="absolute right-0 z-10 w-40 sm:mr-5 px-2 rounded bg-grey peer-checked:block hidden divide-y divide-white divide-opacity-25">
+            @if(Auth::check() && Auth::user()->can('delete', $world))
                 @include('form.delete-world', ['world' => $world])
-                @endif
-                @if(Auth::check() && Auth::user()->can('leave', $world))
-                    <form method="POST" action={{ route('leave-world', ['id' => $world->id, 'username' => Auth::user()->username]) }}>
-                        @CSRF
-                        @method('DELETE')
-                        <button class="px-3 py-1 w-full md:text-medium text-mediumPhone" type="submit">Leave World</button>
-                    </form>
-                @endif
-            </div>
+            @endif
+            @if(Auth::check() && Auth::user()->can('leave', $world))
+                <form method="POST" action={{ route('leave-world', ['id' => $world->id, 'username' => Auth::user()->username]) }}>
+                    @CSRF
+                    @method('DELETE')
+                    <button class="px-3 py-1 w-full md:text-medium text-mediumPhone" type="submit">Leave World</button>
+                </form>
+            @endif
+        </div>
         @endif
     </div>
 </article>
