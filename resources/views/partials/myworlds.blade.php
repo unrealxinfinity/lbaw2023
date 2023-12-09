72fd04b769000bffba917e5d569fff1e68e58707
up202108810 <up202108810@fe.up.pt>
2023-12-09 18:39:44 +0000

    Updated form for ajax

--- resources/views/partials/myworlds.blade.php
+++ resources/views/partials/myworlds.blade.php
@@ -7,21 +7,22 @@
                 <h2 class=""> {{ $world->description }} </h2>
             </div>
         </div>
-        @if(Auth::check() && Auth::user()->can('leave', $world) || Auth::user()->can('delete', $world))
-        <input type="checkbox" id="more-options-{{$world->id}}" class="hidden peer"/>
-        <label for="more-options-{{$world->id}}" class="text-start font-bold md:text-big text-bigPhone h-fit my-3 sm:mr-5 cursor-pointer">&#8942;</label>
-        <div class="absolute right-0 z-10 w-40 sm:mr-5 px-2 rounded bg-grey peer-checked:block hidden divide-y divide-white divide-opacity-25">
-            @can('delete', $world)
-                @include('form.delete-world-in-list', ['world' => $world])
-            @endcan
-            @can('leave', $world)
-                <form method="POST" action={{ route('leave-world', ['id' => $world->id, 'username' => Auth::user()->username]) }}>
-                    @CSRF
-                    @method('DELETE')
-                    <button class="px-3 py-1 w-full md:text-medium text-mediumPhone" type="submit">Leave World</button>
-                </form>
-            @endcan
-        </div>
-        @endif
+        @canany(['delete', 'leave'], $world)
+            <input type="checkbox" id="more-options-{{$world->id}}" class="hidden peer"/>
+            <label for="more-options-{{$world->id}}" class="text-start font-bold md:text-big text-bigPhone h-fit my-3 sm:mr-5 cursor-pointer">&#8942;</label>
+            <div class="absolute right-0 z-10 w-40 sm:mr-5 px-2 rounded bg-grey peer-checked:block hidden divide-y divide-white divide-opacity-25">
+                @can('delete', $world)
+                    @include('form.delete-world-in-list', ['world' => $world])
+                @endcan
+                @can('leave', $world)
+                    <form class="leave-world-list">
+                        @CSRF
+                        <input type="hidden" class="id" name="id" value={{$world_id}}>
+                        <input type="hidden" class="username" name="username" value={{Auth::user()->username}}>
+                        <button class="px-3 py-1 w-full md:text-medium text-mediumPhone" type="submit">Leave World</button>
+                    </form>
+                @endcan
+            </div>
+        @endcanany
     </div>
 </article>
