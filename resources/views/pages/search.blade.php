7ae9f22a897f0ec0dd76eb3814985017c93005bf
isabel <up202108767@up.pt>
2023-12-09 23:46:40 +0000

    search member link missing

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -53,7 +53,7 @@
                 <header class="myworld flex h-fit p-3 mx-1 my-4 bg-black outline outline-1 outline-white/20 rounded">
                     <img src= {{$otherMember->getProfileImage()}} class="mobile:h-14 tablet:h-16 desktop:h-20 h-12 aspect-square">
                     <div class="flex flex-col self-center ml-3 w-11/12">
-                    <h2 class="break-words">{{ $otherMember->name }}</h2>
+                    <h2 class="break-words"><a href="/members/{{ $otherMember->persistentUser->user->username }}">{{ $otherMember->name }}</a></h2>
                     <h3 class="break-words"> @ {{ $otherMember->persistentUser->user->username }}</h3>
                     <h4 class="pt-2 break-words">{{ $otherMember->description }}</h4>
                     </div>
