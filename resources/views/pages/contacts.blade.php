a6ded59326a84fe871ce8482810dd6b9ba590b80
HaoChang Fu <fuhaochang664961423@hotmail.com>
2023-12-11 19:09:22 +0000

    some aditions

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -7,10 +7,10 @@
     <section id="Contacts">
         <div class="bg-grey p-5 rounded-md">
             <h1>Contacts:</h1>
-            <p> Afonso Vaz Osório: up202108700@fe.up.pt </p>
-            <p> Isabel Moutinho: Isabel Moutinho, up202108767@fe.up.pt </p>
-            <p> Tiago Cruz: up202108810@fe.up.pt </p>
-            <p> HaoChang Fu: up202108730@fe.up.pt </p>
+            <p> Afonso Vaz Osório: <a href="mailto:up202108700@fe.up.pt">up202108700@fe.up.pt</a></p>
+            <p> Isabel Moutinho: Isabel Moutinho, <a href="mailto:up202108767@fe.up.pt">up202108767@fe.up.pt</a> </p>
+            <p> Tiago Cruz: <a href="mailto:up202108810@fe.up.pt">up202108810@fe.up.pt </a></p>
+            <p> HaoChang Fu: <a href="mailto:up202108730@fe.up.pt">up202108730@fe.up.pt </a></p>
         </div>
     </section>
     
