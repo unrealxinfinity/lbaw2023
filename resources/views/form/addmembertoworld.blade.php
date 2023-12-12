2e0be30818102be3f8165e4e494a7d8684766787
isabel <up202108767@up.pt>
2023-12-12 12:51:21 +0000

    invite to world change here more visible

--- resources/views/form/addmembertoworld.blade.php
+++ resources/views/form/addmembertoworld.blade.php
@@ -1,7 +1,7 @@
 <form class="form-post" id="invite-member">
     @csrf
     <h2>Invite member. </h2> 
-    <h3>Do you want to invite someone outside MineMax? <span id="invite-outside-member" class="cursor-pointer">Change Here</span></h3>
+    <h3>Do you want to invite someone outside MineMax? <span id="invite-outside-member" class="cursor-pointer text-green underline">Change Here</span></h3>
     <input type="hidden" class="world_id" name="world_id" value="{{ $world->id }}">
     <input type="text" class="username" name="username" placeholder="Username" required>
     <p class="m-0">What role?</p>
@@ -15,7 +15,7 @@
 <form class="form-post hidden" id="invite-new-member">
     @csrf
     <h2>Invite outside member. </h2> 
-    <h3>Do you want to invite someone inside MineMax? <span id="invite-outside-member" class="cursor-pointer">Change Here</span></h3>
+    <h3>Do you want to invite someone inside MineMax? <span id="invite-outside-member" class="cursor-pointer text-green underline">Change Here</span></h3>
     <input type="hidden" class="world_id" name="world_id" value="{{ $world->id }}">
     <input type="text" class="email" name="email" placeholder="example@email.com" required>
     <p class="m-0">What role?</p>
