5a71c76257de37a6f30d4b468b4107553d93c1db
Onso37 <afonso.vo@gmail.com>
2023-12-16 13:43:29 +0000

    fix a bug

--- resources/views/partials/appeal.blade.php
+++ resources/views/partials/appeal.blade.php
@@ -1,7 +1,7 @@
-<form>
+<form class="form-post grid grid-lines-4 grid-cols-2">
     @include('partials.member', ['member' => $appeal->member, 'main' => false, 'appeal' => false])
-    <h3>Reason for appeal</h3>
-    <p>{{ $appeal->text }}</p>
-    <button type="submit" formmethod="POST" formaction="{{ route('unblock-member', ['username' => $appeal->member->persistentUser->user->username]) }}">Accept Appeal</button>
-    <button type="submit" formmethod="POST" formaction="{{ route('deny-appeal', ['id' => $appeal->id]) }}">Deny Appeal</button>
+    <h3 class="row-start-2 col-span-2">Reason for appeal</h3>
+    <p class="row-start-3 col-span-2">{{ $appeal->text }}</p>
+    <button class="button col-span-1" type="submit" formmethod="POST" formaction="{{ route('unblock-member', ['username' => $appeal->member->persistentUser->user->username]) }}">Accept Appeal</button>
+    <button class="button col-span-1" type="submit" formmethod="POST" formaction="{{ route('deny-appeal', ['id' => $appeal->id]) }}">Deny Appeal</button>
 </form>
