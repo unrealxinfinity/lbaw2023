becedfb55641c29e71fef2225b29433a6625b9bf
up202108810 <up202108810@fe.up.pt>
2023-12-16 12:31:11 +0000

    Better about text

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,8 +5,9 @@
 @section('content')
 
     <section id="About">
-       <p> MineMax is a project manager for Minecraft servers. It allows you to create projects, assign tasks to them, and manage them. </p>
-       <p> The team behind MineMax is composed of 4 students from the University of Porto: <a href="/contacts">Afonso Vaz Osório, Isabel Moutinho, Tiago Cruz,HaoChang Fu </a></p>
+        <h2> MineMax is a project management tool to help Minecraft players better organize their Worlds and Projects.</h2>
+        <h2> Our mission is to help Minecraft players to better idealize and organize their projects and have a easy way to look back past the progress they've made. Knowing boredom out of not knowing what to do next is a major problem while playing Minecraft Survival, we seek to help people have more fun and stay enganged during gameplay!</h2>
+        <h2> The team behind MineMax is composed of 4 students from the University of Porto: <a href="/contacts">Afonso Vaz Osório, Isabel Moutinho, Tiago Cruz, HaoChang Fu </a></p>
     </section>
 
 @endsection
