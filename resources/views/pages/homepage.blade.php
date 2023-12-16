6e4dc667516849715dd6e17ebb9c967888357ad9
up202108810 <up202108810@fe.up.pt>
2023-12-16 15:37:03 +0000

    Better visitor homepage

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -3,13 +3,18 @@
 @section('title', 'Home')
 
 @section('content')
-    <section id="homepage" class="mx-5">
-        @unless (Auth::check())
-        <h1>Welcome to MineMax!</h1>
-        <h2>Here you can manage your Worlds and Projects.</p>
-        <h2>Log in to get started!</p>
-        @endunless
-    </section>
+    
+    @unless (Auth::check())
+        <section id="homepage" class="h-screen mt-10 mx-5">
+            <h1 class="text-center">Welcome to MineMax!</h1>
+            <h2 class="text-center">Here you can manage your Worlds and Projects.</h2>
+            <h2 class="text-center">Log in or Register to get started!</h2>
+            <div class="w-full flex justify-center mt-5">
+                <img src="{{ URL('/images/steve-alex-fly.png') }}" alt="Two minecraft player models, Steve, a man, on the left and Alex, a women, on the right, flying using the minecraft item 'elytra' that looks like wings on their back, both holding diamond swords">
+            </div>
+        </section>
+    @endunless
+    
     @php
         $member = Auth::user() ? Auth::user()->persistentUser->member : null;
     @endphp
@@ -20,4 +25,3 @@
         
     
 @endsection
-
