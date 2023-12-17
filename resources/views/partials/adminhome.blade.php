4ef3a8f6669fe6156f58eb727e6473f2cf07e60b
Onso37 <afonso.vo@gmail.com>
2023-12-17 11:32:23 +0000

    fix bugs

--- resources/views/partials/adminhome.blade.php
+++ resources/views/partials/adminhome.blade.php
@@ -1,4 +1,4 @@
 <article class="homepage flex">
-    @include('partials.container', ['description' => "There are $nMembers members currently signed up for MineMax.", 'title' => 'Manage members', 'link' => route('list-members')])
-    @include('partials.container', ['description' => "$nAppeals blocked members wish to appeal their blocks.", 'title' => 'Manage appeals', 'link' => route('admin-appeals')])
+    @include('partials.container', ['description' => "There are $nMembers members currently signed up for MineMax.", 'title' => 'Manage members', 'link' => route('list-members'), 'img' => ""])
+    @include('partials.container', ['description' => "$nAppeals blocked members wish to appeal their blocks.", 'title' => 'Manage appeals', 'link' => route('admin-appeals'), 'img' => ""])
 </article>
