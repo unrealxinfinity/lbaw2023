d0ef9fead41d86a8a2c1961e2405ed53091f716b
up202108810 <up202108810@fe.up.pt>
2023-12-18 18:17:32 +0000

    Favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,12 @@
         <link href="https://fonts.cdnfonts.com/css/minecraft-4" rel="stylesheet">
         <link href="{{ url('css/app.css') }}" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-minimal/minimal.css" rel="stylesheet">
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-chrome-192x192.png">
-        <link rel="icon" type="image/png" sizes="512x512" href="favicon/android-chrome-512x512.png">
-        <link rel="manifest" href="favicon/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png">
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png">
+        <link rel="manifest" href="/favicon/site.webmanifest">
         @vite('public/css/app.css')
         <script type="text/javascript">
             // Fix for Firefox autofocus CSS bug
