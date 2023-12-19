0d3264420f03e60dcce70f664ba707e2001ecaf3
isabel <up202108767@up.pt>
2023-12-19 00:28:59 +0000

    edit world style fix

--- resources/views/form/world-edit.blade.php
+++ resources/views/form/world-edit.blade.php
@@ -1,23 +1,23 @@
 <form method="POST" action="/worlds/upload/{{ $world->id }}" enctype="multipart/form-data">
-    <fieldset>
+    <fieldset class="m-1 my-4 py-2 px-3 w-fit">
         <legend>Upload a new world picture</legend>
         @csrf
         @method('POST')
         
-        <div class="mobile:h-28 tablet:h-32 desktop:h-40 h-20 aspect-square tablet:mx-5 mx-2 my-1">
+        <div class="relative mobile:h-28 tablet:h-32 desktop:h-40 h-24 aspect-square tablet:mx-5 mx-2 my-1">
             <label for="edit-img">
-                <h1 class="absolute mobile:h-28 tablet:h-32 desktop:h-40 h-20 aspect-square text-center flex flex-col justify-around pointer-events-none"><label>&#9998;</label></h1>
-                <img id='preview-img' class="h-full aspect-square hover:opacity-50 object-cover" src={{ $world->getImage() }} alt="{{$world->name}} image">
+                <img id='preview-img' class="absolute h-full aspect-square object-cover hover:opacity-50 cursor-pointer" src={{ $world->getImage() }} alt="{{$world->name}} image">
+                <h1 class="absolute mobile:h-28 tablet:h-32 desktop:h-40 h-24 aspect-square text-center flex flex-col justify-around pointer-events-none"><label>&#9998;</label></h1>
             </label>
         </div>
         <input id="edit-img" class="hidden" name="file" type="file" required>
         <input name="type" type="hidden" value="world">
-        <input class="button tablet:ml-5 ml-2" type="submit" value="Upload world picture">
+        <input class="button px-1 tablet:ml-5 ml-2 mobile:w-28 tablet:w-32 desktop:w-40 w-24" type="submit" value="Upload world picture">
     </fieldset>
 </form>
     
    
-<form class="edit-world form-post outline-none" method="POST" action="{{ route('update-world', ['id' => $world->id]) }}">
+<form class="edit-world form-outline outline-none" method="POST" action="{{ route('update-world', ['id' => $world->id]) }}">
     <fieldset class="form-post">
         <legend>Edit world details</legend>
         @csrf
@@ -25,11 +25,11 @@
 
         <input type="hidden" class="world-id" name="id" value="{{ $world->id }}">
 
-        <label for="world-name">Name</label>
+        <h3><label for="world-name">Name</label></h3>
         <input type="text" class="world-name" id="world-name" name="name" value="{{ $world->name }}" placeholder="Name" required>
 
-        <label for="world-description">Description</label>
-        <textarea type="text" class="world-description text-black max-h-40" id="world-description" rows="4" name="description"  placeholder="Description" required> {{ $world->description }} </textarea>
+        <h3><label for="world-description">Description</label></h3>
+        <textarea type="text" class="world-description text-black max-h-40" id="world-description" rows="3" name="description"  placeholder="Description" required> {{ $world->description }} </textarea>
 
         <input class="button" type="submit" id="submit-{{ $world->id }}" value="Edit World">
     </fieldset>
--- resources/views/pages/world.blade.php
+++ resources/views/pages/world.blade.php
@@ -46,13 +46,15 @@
     <script>
         document.body.style.overflow = 'hidden';
     </script>
-    <div id="edit-world" class="fixed z-30 bg-white bg-opacity-30 top-0 left-0 w-full h-full flex flex-col justify-center scroll">
-        <div class="bg-black tablet:w-3/4 tablet:h-fit h-full tablet:rounded drop-shadow tablet:mx-auto">
-            <div class="flex my-3"> 
-            <h1 class="mt-3 ml-5"> {{ $formTitle }} </h1>
-            <h1><a id="go-back" class="cursor-pointer fixed right-5 mt-1">&times;</a></h1>
+    <div id="edit-world" class="fixed z-30 bg-white bg-opacity-30 top-0 left-0 w-full h-full flex flex-col justify-around">
+        <div class="bg-black tablet:w-3/4 tablet:min-h-fit tablet:max-h-[90%] h-full w-full tablet:rounded tablet:mx-auto">
+            <div class="flex justify-between mx-5 pt-3">
+            <h1> {{ $formTitle }} </h1>
+            <h1><a id="go-back" class="cursor-pointer">&times;</a></h1>
             </div>
+            <div class="overflow-auto tablet:min-h-fit tablet:max-h-[90%] h-[90%]">
             @include($formName, ['world'=>$world])
+            </div>
         </div>
     </div>
     @endif
