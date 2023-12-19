803afdf5cdcf724b15fc7f7ecbcaa8bbf33aed99
Onso37 <afonso.vo@gmail.com>
2023-12-19 19:03:25 +0000

    improve look

--- resources/views/partials/appeal-readonly.blade.php
+++ resources/views/partials/appeal-readonly.blade.php
@@ -1,8 +1,10 @@
-@include('partials.member', ['member' => $member, 'main' => false, 'appeal' => false])
-<h2>Reason for appeal</h2>
-<p>{{ $member->appeal->text }}</p>
-@if ($member->appeal->denied)
-    <h2>Your appeal has been denied</h2>
-@else
-    <h2>Your appeal is under review</h2>
-@endif
+
+<div class="grid grid-lines-4 grid-cols-1 gap-4 p-1.5">
+    <h2 class="justify-self-center">Reason for appeal</h2>
+    <p class="justify-self-center">{{ $member->appeal->text }}</p>
+    @if ($member->appeal->denied)
+        <h2 class="bg-mine-red justify-self-center p-3">Your appeal has been denied</h2>
+    @else
+        <h2 class="bg-mine-red justify-self-center p-3">Your appeal is under review</h2>
+    @endif
+</div>
