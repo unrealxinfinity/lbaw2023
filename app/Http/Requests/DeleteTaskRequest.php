488908172e85ac8f2e1204f07c01fdaf6f88f5cf
up202108810 <up202108810@fe.up.pt>
2023-12-20 23:32:36 +0000

    Delete Task Request

---
+++ app/Http/Requests/DeleteTaskRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task;
+
+class DeleteTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user()->can('delete', Task::find($this->route()->id));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
