bb567f8c5f5e6951287f17d5f7dde78caae4d2e9
isabel <up202108767@up.pt>
2023-12-20 16:06:29 +0000

    faqs responsive

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <section id="Contacts" class="bg-white/90 p-5 rounded-md desktop:w-1/2 tablet:w-2/3 m-auto">
+    <section id="contacts" class="bg-white/90 p-5 rounded-md desktop:w-1/2 tablet:w-2/3 m-auto">
         <h1 class="text-black bold">Contacts us via email:</h1>
         <ul class="list-disc pl-5 text-black">
             <li> Afonso Vaz Osório: <a href="mailto:up202108700@up.pt" class="hover:underline">up202108700@up.pt</a></li>
