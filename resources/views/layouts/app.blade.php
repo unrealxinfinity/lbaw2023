cb0af7bea0e47a66c870e257a7e200e9166da1b4
Onso37 <afonso.vo@gmail.com>
2023-12-20 11:56:27 +0000

    pusher is self hosted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
             // Fix for Firefox autofocus CSS bug
             // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
         </script>
-        <script src="https://js.pusher.com/7.0/pusher.min.js" defer></script>
+        <script src={{ url('js/pusher.js') }} defer></script>
         <script src={{ url('js/sweetalert.js') }}></script>
         <script type="text/javascript" src={{ url('js/app.js') }} defer></script>
         
