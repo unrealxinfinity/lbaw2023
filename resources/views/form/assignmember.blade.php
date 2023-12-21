c956e407e0bab7ae067db03b474aef366b2d9f5a
Onso37 <afonso.vo@gmail.com>
2023-12-21 19:19:52 +0000

    more validation

--- resources/views/form/assignmember.blade.php
+++ resources/views/form/assignmember.blade.php
@@ -4,10 +4,10 @@
         @csrf
         <input type="hidden" class="id" name="id" value="{{ $task->id }}">
     
-        <h3 class="my-0 mt-3 col-span-2"> <label for="username">Username <b class="text-red">*</b></label> </h3>
-        <input type="text" class="username" name="username" placeholder="Username" required tabindex="0">
+        <h3 class="my-0 mt-3 col-span-2"> <label for="{{$prefix . 'username'}}">Username <b class="text-red">*</b></label> </h3>
+        <input type="text" id="{{$prefix . 'username'}}" class="username" name="username" placeholder="Username" required tabindex="0">
        
-        <label for="submit-assign" hidden>Submit assign member</label>
-        <input class="button" type="submit" id="submit-assign" value="Assign member" tabindex="0">
+        <label for="{{$prefix . 'submit-assign'}}" hidden>Submit assign member</label>
+        <input class="button" type="submit" id="{{$prefix . 'submit-assign'}}" value="Assign member" tabindex="0">
     </fieldset>
 </form>
